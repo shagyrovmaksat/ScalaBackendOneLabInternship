689c61c0c7c9358ad93b40bd2ff33d3c8e717add
shagyrovmaksat <maksat.omar@mail.ru>
2021-06-07 11:24:14 +0500

    ping pong

---
+++ hw1/src/main/scala/ping/pong/actors/Ping.scala
@@ -0,0 +1,27 @@
+package ping.pong.actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+
+class Ping(pong: ActorRef) extends Actor with ActorLogging {
+  var count = 0
+  def increment = count += 1
+
+  override def receive: Receive = {
+    case a@"Start" =>
+      log.info(s"$a count = $count")
+
+      increment
+      pong ! "Ping"
+
+    case a@"Pong" =>
+      log.info(s"$a count = $count")
+
+      increment
+      if (count >= 100) {
+        sender ! "Stop"
+        context.stop(self)
+      } else {
+        sender ! "Ping"
+      }
+  }
+}
